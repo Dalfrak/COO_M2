a7ca9e40c85ff348ff78627e1a2d7bea669b5f74
Dalfrak <blitzhacker31@gmail.com>
2021-10-04 20:35:38 +0200

    C'est mieux ainsi

--- src/model/Step.java
+++ src/model/Step.java
@@ -7,12 +7,11 @@ public class Step extends Block {
 	private double initialValue;
 	private double finalValue;
 
-	public Step(String id, TreeMap<String, Data> X, TreeMap<String, Data> Y, double ta, double initialValue,
-			double finalValue) {
+	public Step(String id, TreeMap<String, Data> X, TreeMap<String, Data> Y, double ta, double initialValue, double finalValue) {
 		super(id, X, Y, ta);
 		this.initialValue = initialValue;
-		this.finalValue = finalValue;
-		this.tr = 0;
+		this.finalValue   = finalValue;
+		this.tr           = 0;
 		this.outputEvents.put(this.id, new Data(this.initialValue));
 	}
 
@@ -27,8 +26,11 @@ public class Step extends Block {
 
 	@Override
 	public void output() {
-		if (this.currentState == 2)
+		System.out.println(this.id + ".output()");
+		if (this.currentState == 2) {
 			this.outputEvents.put(this.id, new Data(this.finalValue));
+			System.out.println("\tPut " + this.finalValue + " into output events list");
+		}
 	}
 
 	@Override
